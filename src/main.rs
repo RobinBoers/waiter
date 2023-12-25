db714aba52f9a8450dc722b054de23e4f15915a0
Robin Boers <robindev2019@outlook.com>
2023-12-25 17:01:14 +0100

    Update to from hyper 1.0 RC to final release

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
+use hyper_util::rt::TokioIo;
 use std::convert::Infallible;
 use tokio::net::TcpListener;
 
@@ -42,9 +43,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     loop {
         let (stream, _) = listener.accept().await?;
 
+        let io = TokioIo::new(stream);
+
         tokio::task::spawn(async move {
             if let Err(err) = http1::Builder::new()
-                .serve_connection(stream, service_fn(move |req| handle_request(req, args.dev)))
+                .serve_connection(io, service_fn(move |req| handle_request(req, args.dev)))
                 .await
             {
                 println!("Error serving connection: {:?}", err);
